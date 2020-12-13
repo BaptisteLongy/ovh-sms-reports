7f8516b72b98cb3bc10350ed5938818dbdb66c59
Baptiste Longy <baptiste.longy@gmail.com>
2020-12-13 20:46:49 +0100

    feat: check new version availability

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import SmsConsumptionReport from './sms-comsuption-report/smsConsumptionReport'
+import fetch from 'node-fetch'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [lastestTag, setLastestTag] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://api.github.com/repos/BaptisteLongy/ovh-sms-reports/tags');
+      const json = await response.json();
+      setLastestTag(json[0].name.substring(1));
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-        <SmsConsumptionReport />
+      {lastestTag && lastestTag !== process.env.REACT_APP_VERSION ? <p>Il y a une nouvelle version, fais un git pull !</p> : null}
+      <SmsConsumptionReport />
     </div>
   );
 }
